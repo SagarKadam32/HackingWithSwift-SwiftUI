b4353addc5a39f4d3d94891ecf1b51a4c84be449
Sagar Kadam <>
2022-07-29 02:53:26 +0530

    Initial Commit
    Reading text from the user with TextField
    https://www.hackingwithswift.com/books/ios-swiftui/reading-text-from-the-user-with-textfield

---
+++ NewWeSplit/NewWeSplit/NewWeSplit/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  NewWeSplit
+//
+//  Created by Sagar Kadam on 29/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10,15,20,25,0]
+    var body: some View {
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            
+            Section {
+                Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewWeSplit/NewWeSplit/NewWeSplit/NewWeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewWeSplitApp.swift
+//  NewWeSplit
+//
+//  Created by Sagar Kadam on 29/07/22.
+//
+
+import SwiftUI
+
+@main
+struct NewWeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
