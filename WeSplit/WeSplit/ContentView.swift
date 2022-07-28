a7a9415dfb0007e9e36ceaa465da68b0dd094f53
Sagar Kadam <>
2022-07-28 18:19:54 +0530

    Adding a navigation bar

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,25 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form {
-            Section {
-                Text("Hello, world 1")
-                Text("Hello, world 2")
-                Text("Hello, world 3")
-                Text("Hello, world 4")
-                Text("Hello, world 5")
+        NavigationView{
+            Form {
+                Section {
+                    Text("Hello, world 1")
+                    Text("Hello, world 2")
+                    Text("Hello, world 3")
+                    Text("Hello, world 4")
+                    Text("Hello, world 5")
+                }
+                
+                Section {
+                    Text("Hello, world 6")
+                    Text("Hello, world 7")
+                    Text("Hello, world 8")
+                    Text("Hello, world 9")
+                    Text("Hello, world 10")
+                    Text("Hello, world 11")
+                }
             }
-            
-            Section {
-                Text("Hello, world 6")
-                Text("Hello, world 7")
-                Text("Hello, world 8")
-                Text("Hello, world 9")
-                Text("Hello, world 10")
-                Text("Hello, world 11")
-            }
+            .navigationTitle("Swift UI")
+            .navigationBarTitleDisplayMode(.inline)
         }
-
     }
 }
 
