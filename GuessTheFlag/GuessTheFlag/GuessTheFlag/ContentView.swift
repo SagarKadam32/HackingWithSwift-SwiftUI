c507fef703c59fbf41d1dc0cd182751c3fff34eb
Sagar Kadam <>
2022-07-30 15:20:04 +0530

    Initial commit
    Using stacks to arrange views

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Sagar Kadam on 30/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            HStack {
+                Text("1")
+                Text("2")
+                Text("3")
+            }
+            HStack {
+                Text("4")
+                Text("5")
+                Text("6")
+            }
+            HStack {
+                Text("7")
+                Text("8")
+                Text("9")
+            }
+        }
+     
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Sagar Kadam on 30/07/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
