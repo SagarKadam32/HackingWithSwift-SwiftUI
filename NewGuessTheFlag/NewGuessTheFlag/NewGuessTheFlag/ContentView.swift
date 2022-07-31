d20605b3c2b3d13772edfe89a9d256e253d7a412
Sagar Kadam <>
2022-07-31 18:20:20 +0530

    Challenge-3 Make the game show only 8 questions, at which point they see a final alert judging their score and can restart the game.

--- NewGuessTheFlag/NewGuessTheFlag/NewGuessTheFlag/ContentView.swift
+++ NewGuessTheFlag/NewGuessTheFlag/NewGuessTheFlag/ContentView.swift
@@ -13,78 +13,85 @@ struct ContentView: View {
     @State private var currentScore = 0
     @State private var isWrongAnswer = false
     @State private var currentSelectedIndex = 0
+    @State private var shouldRestartGame = false
+    @State private var numberOfQuestionsAksed = 0
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia","Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     var body: some View {
-        
         ZStack {
-            
-            RadialGradient(stops: [
-                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
-                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
-            ], center: .top, startRadius: 200, endRadius: 700)
-                .ignoresSafeArea()
-            
-            VStack{
-                VStack {
-                    Spacer()
-                    Text("Guess the Flag")
-                    .font(.largeTitle.weight(.bold))
-                    .foregroundColor(.white)
-           
-                    VStack(spacing: 15) {
-                        VStack {
-                            Text("Tap the flag of")
-                                .foregroundColor(.secondary)
-                                .font(.subheadline.weight(.heavy))
-                            Text(countries[correctAnswer])
-                                .font(.largeTitle.weight(.semibold))
-                        }
-                        
-                        ForEach(0..<3) { number in
-                            Button {
-                                currentSelectedIndex = number
-                                isWrongAnswer = flagTappged(currentSelectedIndex)
-                                
-                            } label: {
-                                    Image(countries[number])
-                                    .renderingMode(.original)
-                                    .clipShape(Rectangle())
-                                    .shadow(color: .black, radius: 5)
+                RadialGradient(stops: [
+                    .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                    .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+                ], center: .top, startRadius: 200, endRadius: 700)
+                    .ignoresSafeArea()
+                
+                VStack{
+                    VStack {
+                        Spacer()
+                        Text("Guess the Flag")
+                        .font(.largeTitle.weight(.bold))
+                        .foregroundColor(.white)
+               
+                        VStack(spacing: 15) {
+                            VStack {
+                                Text("Tap the flag of")
+                                    .foregroundColor(.secondary)
+                                    .font(.subheadline.weight(.heavy))
+                                Text(countries[correctAnswer])
+                                    .font(.largeTitle.weight(.semibold))
+                            }
+                            
+                            ForEach(0..<3) { number in
+                                Button {
+                                    currentSelectedIndex = number
+                                    isWrongAnswer = flagTappged(currentSelectedIndex)
+                                    
+                                } label: {
+                                        Image(countries[number])
+                                        .renderingMode(.original)
+                                        .clipShape(Rectangle())
+                                        .shadow(color: .black, radius: 5)
+                                }
                             }
                         }
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 20)
+                        .background(.regularMaterial)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                    
+                        Spacer()
+                        Spacer()
+                    
+                        Text("Score: \(currentScore)")
+                        .foregroundColor(.white)
+                        .font(.title.bold())
+                    
+                        Spacer()
                     }
-                    .frame(maxWidth: .infinity)
-                    .padding(.vertical, 20)
-                    .background(.regularMaterial)
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                
-                    Spacer()
-                    Spacer()
-                
-                    Text("Score: \(currentScore)")
-                    .foregroundColor(.white)
-                    .font(.title.bold())
-                
-                    Spacer()
-                }
-                .alert("Incorrect Answer!!!", isPresented: $isWrongAnswer) {
-                    Button("Re-Try ?", action: askQuestion)
+                    .alert("Incorrect Answer!!!", isPresented: $isWrongAnswer) {
+                        Button("Re-Try ?", action: askQuestion)
+                    } message: {
+                        Text("This is \(countries[currentSelectedIndex])'s Flag")
+                    }
+                }.padding()
+            }
+            .alert(scoreTitle, isPresented: $shwoingScore) {
+                    Button("Continue", action: askQuestion)
                 } message: {
-                    Text("This is flag of \(countries[currentSelectedIndex])")
+                    Text("Your score is = \(currentScore)")
                 }
-            }.padding()
+            .alert(scoreTitle, isPresented: $shouldRestartGame) {
+                        Button("Restart Game", action: resetGame)
+                    } message: {
+                        Text("Your have reached maximum limit of 8 Questions & your score is = \(currentScore)")
+                    }
         }
-        .alert(scoreTitle, isPresented: $shwoingScore) {
-            Button("Continue", action: askQuestion)
-        } message: {
-            Text("Your score is \(currentScore)")
-        }
-        }
+    
     
     func flagTappged(_ number: Int) -> Bool {
         var isWrongAnswer = false
+        numberOfQuestionsAksed += 1
         if number == correctAnswer {
             scoreTitle = "Correct"
             currentScore += 1
@@ -94,12 +101,24 @@ struct ContentView: View {
             currentScore -= 1
             isWrongAnswer = true
          }
+        if(numberOfQuestionsAksed == 8){
+            shouldRestartGame = true
+        }
         return isWrongAnswer
     }
     
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func resetGame() {
+        currentScore = 0
+        numberOfQuestionsAksed = 0
+        shwoingScore = false
+        isWrongAnswer = false
+        shouldRestartGame = false
+        askQuestion()
     }
 }
 
