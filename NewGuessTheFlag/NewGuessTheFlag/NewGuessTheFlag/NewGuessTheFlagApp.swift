df4f9410c27ec49ce1fe1d01ac30fc5fcdc2f8ba
Sagar Kadam <>
2022-07-31 15:36:15 +0530

    Initial Commit

---
+++ NewGuessTheFlag/NewGuessTheFlag/NewGuessTheFlag/NewGuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewGuessTheFlagApp.swift
+//  NewGuessTheFlag
+//
+//  Created by Sagar Kadam on 31/07/22.
+//
+
+import SwiftUI
+
+@main
+struct NewGuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
