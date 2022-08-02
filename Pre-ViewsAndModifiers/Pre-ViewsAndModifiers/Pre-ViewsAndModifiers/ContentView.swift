535094a56eaf758efd3854e3a389dcf8b775261e
Sagar Kadam <>
2022-08-03 01:17:43 +0530

    Why modifier order matters https://www.hackingwithswift.com/books/ios-swiftui/why-modifier-order-matters

---
+++ Pre-ViewsAndModifiers/Pre-ViewsAndModifiers/Pre-ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Pre-ViewsAndModifiers
+//
+//  Created by Sagar Kadam on 03/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            Button("Tap Me") {
+                print(type(of: self.body))
+            }
+            .background(.red)
+            .foregroundColor(.white)
+            .frame(width: 200, height: 100)
+            .background(.blue)
+            
+            Text("Color me")
+                .padding()
+                .background(.red)
+                .padding()
+                .background(.blue)
+                .padding()
+                .background(.green)
+                .padding()
+                .background(.yellow)
+            
+        }
+
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pre-ViewsAndModifiers/Pre-ViewsAndModifiers/Pre-ViewsAndModifiers/Pre_ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pre_ViewsAndModifiersApp.swift
+//  Pre-ViewsAndModifiers
+//
+//  Created by Sagar Kadam on 03/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Pre_ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
