2010b163d5fee3c66e0f41ef9f17ab97b053b8f3
Sagar Kadam <>
2022-08-06 19:50:09 +0530

    Connecting SwiftUI to Core ML https://www.hackingwithswift.com/books/ios-swiftui/connecting-swiftui-to-core-ml

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -5,47 +5,77 @@
 //  Created by Sagar Kadam on 06/08/22.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
     var body: some View {
         
         NavigationView {
             VStack {
-                HStack {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
-                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                        .labelsHidden()
-                }
-                
-                HStack(alignment: .center) {
-                    Text("Desired amount of sleep")
-                        .font(.headline)
+                    HStack {
+                        Text("When do you want to wake up?")
+                            .font(.headline)
+                        DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                            .labelsHidden()
+                    }
                     
-                    Stepper("\(sleepAmount.formatted()) Hours", value: $sleepAmount, in: 4...12, step: 0.25)
-                }
-                
-                HStack(alignment: .center){
-                    Text("Daily coffee intake")
-                        .font(.headline)
+                    HStack(alignment: .center) {
+                        Text("Desired amount of sleep")
+                            .font(.headline)
+                        
+                        Stepper("\(sleepAmount.formatted()) Hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                    }
                     
-                    Stepper(coffeeAmount == 1 ? "1 cup": "\(coffeeAmount) cups",value: $coffeeAmount, in: 1...20)
+                    HStack(alignment: .center){
+                        Text("Daily coffee intake")
+                            .font(.headline)
+                        
+                        Stepper(coffeeAmount == 1 ? "1 cup": "\(coffeeAmount) cups",value: $coffeeAmount, in: 1...20)
+                    }
                 }
-            }
-            .padding()
-            .navigationTitle("BetterRest")
-            .toolbar{
-                    Button("Calculate", action: calculateBedTime)
-            }
+                .padding()
+                .navigationTitle("BetterRest")
+                .toolbar{
+                        Button("Calculate", action: calculateBedTime)
+                }
+                .alert(alertTitle, isPresented: $showingAlert) {
+                    Button("OK") {}
+                }message: {
+                    Text(alertMessage)
+                }
         }
     }
     
     func calculateBedTime() {
         
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        }catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was as problem calculating your bedtime."
+        }
+        showingAlert = true
     }
 }
 
