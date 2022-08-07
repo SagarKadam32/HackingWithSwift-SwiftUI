59eb3538f1cc7cd39c167bba99fb557d1f045209
Sagar Kadam <>
2022-08-07 13:12:08 +0530

    Challenge-3 :- Change the user interface so that it always shows their recommended bedtime using a nice and large font. You should be able to remove the “Calculate” button entirely.

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    @State private var idealBedTime = ""
     
     @State private var alertTitle = ""
     @State private var alertMessage = ""
@@ -33,6 +34,9 @@ struct ContentView: View {
                             .font(.headline)
                         DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                             .labelsHidden()
+                            .onChange(of: wakeUp) { newValue in
+                                calculateBedTime()
+                            }
                     }
                     
                     VStack(alignment: .leading, spacing: 5) {
@@ -40,36 +44,56 @@ struct ContentView: View {
                             .font(.headline)
                         
                         Stepper("\(sleepAmount.formatted()) Hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                            .onChange(of: sleepAmount) { newValue in
+                                calculateBedTime()
+                            }
                     }
-                    
-                VStack(alignment: .leading, spacing: 5){
-                        Text("Daily coffee intake")
-                            .font(.headline)
-                        /*
-                        Stepper(coffeeAmount == 1 ? "1 cup": "\(coffeeAmount) cups",value: $coffeeAmount, in: 1...20) */
-                    
-                        Picker("Number of Cups", selection: $coffeeAmount) {
-                            ForEach(1..<21){
-                                Text("\($0)")
+                   
+                    VStack(alignment: .leading, spacing: 5){
+                            Text("Daily coffee intake")
+                                .font(.headline)
+                            /*
+                            Stepper(coffeeAmount == 1 ? "1 cup": "\(coffeeAmount) cups",value: $coffeeAmount, in: 1...20)
+                             */
+                        
+                            Picker("Number of Cups", selection: $coffeeAmount) {
+                                ForEach(1..<21){
+                                    Text("\($0)")
+                                }
+                            }
+                            .onChange(of: coffeeAmount) { newValue in
+                                calculateBedTime()
                             }
                         }
+                
+                    Section(){
+                        Text("\(idealBedTime)")
+                            .font(.largeTitle)
+                            .onAppear(){
+                                calculateBedTime()
+                            }
+                           
+                    }header: {
+                        Text("Your IDEAL BED TIME")
+                            .font(.headline)
                     }
-                }
-                .padding()
-                .navigationTitle("BetterRest")
-                .toolbar{
-                        Button("Find Ideal BedTime ?", action: calculateBedTime)
-                }
-                .alert(alertTitle, isPresented: $showingAlert) {
-                    Button("OK") {}
-                }message: {
-                    Text(alertMessage)
-                }
-        }
+                    }
+                    .padding()
+                    .navigationTitle("BetterRest")
+                    /*
+                    .toolbar{
+                            Button("Find Ideal BedTime ?", action: calculateBedTime)
+                    }
+                    .alert(alertTitle, isPresented: $showingAlert) {
+                        Button("OK") {}
+                    }message: {
+                        Text(alertMessage)
+                    }
+                     */
+            }
     }
     
     func calculateBedTime() {
-        
         do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
@@ -83,7 +107,8 @@ struct ContentView: View {
             let sleepTime = wakeUp - prediction.actualSleep
             
             alertTitle = "Your ideal bedtime is..."
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            //alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            idealBedTime = sleepTime.formatted(date: .omitted, time: .shortened)
         }catch {
             alertTitle = "Error"
             alertMessage = "Sorry, there was as problem calculating your bedtime."
