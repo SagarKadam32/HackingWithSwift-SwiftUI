1674ba91c70849497702561316c5b279fc8580b7
Sagar Kadam <>
2022-08-10 08:34:15 +0530

    Creating implicit animations https://www.hackingwithswift.com/books/ios-swiftui/creating-implicit-animations

---
+++ Pre-Animations/Pre-Animations/Pre-Animations/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Pre-Animations
+//
+//  Created by Sagar Kadam on 10/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var animationAmount = 1.0
+    var body: some View {
+        
+        Button("Tap Me"){
+            animationAmount += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount-1) * 3 )
+        .animation(.default, value: animationAmount)
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pre-Animations/Pre-Animations/Pre-Animations/Pre_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pre_AnimationsApp.swift
+//  Pre-Animations
+//
+//  Created by Sagar Kadam on 10/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Pre_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
