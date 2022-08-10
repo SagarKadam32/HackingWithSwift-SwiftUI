ed8178e19f8ad6ebd12ced5576640e62eba0edb5
Sagar Kadam <>
2022-08-10 20:01:02 +0530

    Animations: Basics Creating implicit animations Customizing animations in SwiftUI Animating bindings Creating explicit animations Advanced Usage Controlling the animation stack

--- Pre-Animations/Pre-Animations/Pre-Animations/ContentView.swift
+++ Pre-Animations/Pre-Animations/Pre-Animations/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var animationAmount = 1.0
+    @State private var enabled = false
+    
     var body: some View {
         
+        /*
         Button("Tap Me"){
             animationAmount += 1
         }
@@ -20,8 +23,81 @@ struct ContentView: View {
         .clipShape(Circle())
         .scaleEffect(animationAmount)
         .blur(radius: (animationAmount-1) * 3 )
-        .animation(.default, value: animationAmount)
+        /*
+        .animation(.easeInOut(duration: 2)
+                    .delay(1),
+                   value: animationAmount) */
+        /*
+        .animation(
+            .easeInOut(duration: 1)
+                .repeatCount(3, autoreverses: true),
+            value: animationAmount
+        ) */
+        /*
+        .animation(
+            .easeInOut(duration: 1)
+                .repeatForever(autoreverses: true),
+            value: animationAmount
+        ) */
+        /*
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+        )*/
+        
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                    value: animationAmount
+                )
+        )
+        */
+        
+        /*
+        VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
+            Spacer()
             
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundColor(Color(.white))
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+
+        }*/
+        /*
+        Button("Tap Me") {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                animationAmount += 360
+            }
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+         */
+        
+        Button("Tap Me") {
+            enabled.toggle()
+        }
+        .frame(width: 200, height: 200)
+        .background(enabled ? .blue : .red)
+        .animation(.default, value: enabled)
+        .foregroundColor(.white)
+        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60: 0))
+        .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
+
     }
 }
 
