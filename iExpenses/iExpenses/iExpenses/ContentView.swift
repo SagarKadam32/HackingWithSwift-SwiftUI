3b69ba96ba4ebc3fd54b5cb472363065e7ea81e6
Sagar Kadam <>
2022-08-12 17:18:44 +0530

    Sharing an observed object with a new view https://www.hackingwithswift.com/books/ios-swiftui/sharing-an-observed-object-with-a-new-view

--- iExpenses/iExpenses/iExpenses/ContentView.swift
+++ iExpenses/iExpenses/iExpenses/ContentView.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var expenses = Expenses()
+    @State private var showingAddExpense = false
     
     var body: some View {
-        
         NavigationView {
             List {
                 ForEach(expenses.items, id: \.id) { item in
@@ -22,14 +22,15 @@ struct ContentView: View {
             .navigationTitle("iExpense")
             .toolbar {
                 Button {
-                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
-                    expenses.items.append(expense)
+                    showingAddExpense = true
                 }label: {
                     Image(systemName: "plus")
                 }
             }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
+            }
         }
-
     }
     
     func removeItems(at offsets: IndexSet) {
