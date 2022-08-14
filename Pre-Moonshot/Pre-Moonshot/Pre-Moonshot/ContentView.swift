c503da61eb0977c1ecac9966250c287cec1b56bd
Sagar Kadam <>
2022-08-14 23:04:17 +0530

    How ScrollView lets us work with scrolling data https://www.hackingwithswift.com/books/ios-swiftui/how-scrollview-lets-us-work-with-scrolling-data

--- Pre-Moonshot/Pre-Moonshot/Pre-Moonshot/ContentView.swift
+++ Pre-Moonshot/Pre-Moonshot/Pre-Moonshot/ContentView.swift
@@ -7,15 +7,44 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(_ text: String) {
+        print("Creating a new CustomText-\(text)")
+        self.text = text
+    }
+}
+
+
 struct ContentView: View {
     var body: some View {
-        GeometryReader { geo in
-            Image("Swiss")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 300, height: 300)
-                .frame(width: geo.size.width, height: geo.size.height
-                )
+        ScrollView(){
+            LazyVStack(spacing: 10) {
+                /*
+                GeometryReader { geo in
+                    Image("Swiss")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 300)
+                        .frame(width: geo.size.width, height: geo.size.height
+                        )
+                }*/
+                
+                ForEach(0..<100) {
+                    /*
+                    Text("Item \($0)")
+                        .font(.title)
+                     */
+                    CustomText("Item \($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
         }
     }
 }
