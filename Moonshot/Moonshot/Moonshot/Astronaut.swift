83fac1524d7205075ba5673660451c4264f85b3c
Sagar Kadam <>
2022-08-15 16:20:31 +0530

    Loading a specific kind of Codable data
    https://www.hackingwithswift.com/books/ios-swiftui/loading-a-specific-kind-of-codable-data

---
+++ Moonshot/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Sagar Kadam on 15/08/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
