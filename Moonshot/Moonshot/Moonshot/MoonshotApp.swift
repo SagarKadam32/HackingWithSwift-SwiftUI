5748769c4d724f215c321a71df99e202e46ba3e2
Sagar Kadam <>
2022-08-15 16:11:57 +0530

    Initial Commit :- Project 8: Moonshot Teach users about space history with scroll views, Codable, and more

---
+++ Moonshot/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Sagar Kadam on 15/08/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
