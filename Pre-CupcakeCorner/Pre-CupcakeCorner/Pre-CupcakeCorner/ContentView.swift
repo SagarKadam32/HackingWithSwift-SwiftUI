1c4c2195ddc4e9b735c1c38cb05b7816cca3beda
Sagar Kadam <>
2022-08-17 09:14:48 +0530

    Adding Codable conformance for @Published properties https://www.hackingwithswift.com/books/ios-swiftui/adding-codable-conformance-for-published-properties

---
+++ Pre-CupcakeCorner/Pre-CupcakeCorner/Pre-CupcakeCorner/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Pre-CupcakeCorner
+//
+//  Created by Sagar Kadam on 17/08/22.
+//
+
+import SwiftUI
+
+class User: ObservableObject, Codable {
+    @Published var name = "Sagar Kadam"
+    
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pre-CupcakeCorner/Pre-CupcakeCorner/Pre-CupcakeCorner/Pre_CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pre_CupcakeCornerApp.swift
+//  Pre-CupcakeCorner
+//
+//  Created by Sagar Kadam on 17/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Pre_CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
