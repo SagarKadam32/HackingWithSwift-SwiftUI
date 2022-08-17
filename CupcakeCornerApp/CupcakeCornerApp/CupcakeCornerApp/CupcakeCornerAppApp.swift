fa84df22c5f5255333d5281e72bbc299ec0a2e11
Sagar Kadam <>
2022-08-17 11:30:37 +0530

    Initial Code Sending and receiving Codable data with URLSession and SwiftUI

---
+++ CupcakeCornerApp/CupcakeCornerApp/CupcakeCornerApp/CupcakeCornerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerAppApp.swift
+//  CupcakeCornerApp
+//
+//  Created by Sagar Kadam on 17/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
