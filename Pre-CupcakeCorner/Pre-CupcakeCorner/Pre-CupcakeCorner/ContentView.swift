9cd10ea631322ee7293c2f6e568889b98862c03d
Sagar Kadam <>
2022-08-17 20:11:04 +0530

    Loading an image from a remote server https://www.hackingwithswift.com/books/ios-swiftui/loading-an-image-from-a-remote-server

--- Pre-CupcakeCorner/Pre-CupcakeCorner/Pre-CupcakeCorner/ContentView.swift
+++ Pre-CupcakeCorner/Pre-CupcakeCorner/Pre-CupcakeCorner/ContentView.swift
@@ -27,8 +27,34 @@ class User: ObservableObject, Codable {
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"))
+//        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"),scale: 3)
+        
+        /*
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
+            image
+                .resizable()
+                .scaledToFit()
+        } placeholder: {
+           // Color.red
+            ProgressView()
+        }
+        .frame(width: 200, height: 200)
+        */
+        
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            }else if phase.error != nil {
+                Text("There was error loading the page")
+            }else {
+                ProgressView()
+            }
+        }
+        .frame(width:200, height: 200)
+        
     }
 }
 
