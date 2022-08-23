b0dc95df2a644729e53ee10c1b5a944d2c2b775d
Sagar Kadam <>
2022-08-23 08:34:13 +0530

    Creating a custom component with @Binding

---
+++ Pre-Bookworm/Pre-Bookworm/Pre-Bookworm/Pre_BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pre_BookwormApp.swift
+//  Pre-Bookworm
+//
+//  Created by Sagar Kadam on 23/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Pre_BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
