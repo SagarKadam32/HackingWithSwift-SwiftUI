5aa73dfb5a1b176827e8736d35dfcd6099a1eb05
Sagar Kadam <>
2022-08-25 09:44:17 +0530

    Filtering @FetchRequest using NSPredicate

--- CoreDataProject/CoreDataProject/CoreDataProject/ContentView.swift
+++ CoreDataProject/CoreDataProject/CoreDataProject/ContentView.swift
@@ -10,9 +10,17 @@ import SwiftUI
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
     @FetchRequest(sortDescriptors: []) var wizards: FetchedResults<Wizard>
+    @FetchRequest(sortDescriptors: []) var ships: FetchedResults<Ship>
+    
+    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe == 'Star Wars'")) var starWarsUniverseShips: FetchedResults<Ship>
+    
+    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "name BEGINSWITH[c] %@", "e")) var shipNamesStartswithE: FetchedResults<Ship>
+
+    
     var body: some View {
         
         /* Ensuring Core Data objects are unique using constraints */
+        /*
         VStack {
             List(wizards, id: \.self) { wizard in
                 Text(wizard.name ?? "Unknown")
@@ -32,7 +40,64 @@ struct ContentView: View {
                     print(error.localizedDescription)
                 }
             }
-        }
+        } */
+        
+        /* Filtering @FetchRequest using NSPredicate */
+            VStack {
+                Form {
+                    Section("Original Data") {
+                        List(ships, id:\.self) { ship in
+                            VStack(alignment: .leading) {
+                                Text(ship.name ?? "Unknown name")
+                                Text(ship.universe ?? "TBD")
+                            }
+                        }
+                    }
+                    
+                    Section("Star Wars Ships") {
+                        List(starWarsUniverseShips, id:\.self) { ship in
+                            VStack(alignment: .leading)  {
+                                Text(ship.name ?? "Unknown name")
+                                Text(ship.universe ?? "TBD")
+                            }                        }
+                    }
+                    
+                    Section("Ship Name with E") {
+                        List(shipNamesStartswithE, id:\.self) { ship in
+                            VStack(alignment: .leading) {
+                                Text(ship.name ?? "Unknown name")
+                                Text(ship.universe ?? "TBD")
+                            }
+                        }
+                    }
+                
+                }
+                
+
+                
+                Button("Add Examples") {
+                    
+                    let ship1 = Ship(context: moc)
+                    ship1.name = "Enterprise"
+                    ship1.universe = "Star Trek"
+                    
+                    let ship2 = Ship(context: moc)
+                    ship2.name = "Defiant"
+                    ship2.universe = "Star Trek"
+                    
+                    let ship3 = Ship(context: moc)
+                    ship3.name = "Millennium Falcon"
+                    ship3.universe = "Star Wars"
+                    
+                    let ship4 = Ship(context: moc)
+                    ship4.name = "Executor"
+                    ship4.universe = "Star Wars"
+                    
+                    if moc.hasChanges {
+                        try? moc.save()
+                    }
+                }
+            }
     }
 }
 
