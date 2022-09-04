aef3ebffe5f06a24aa7ea01f9fbeb462697a59c7
Sagar Kadam <>
2022-09-04 18:08:13 +0530

    Creating tabs with TabView and tabItem()

--- Pre-HotProspects/Pre-HotProspects/Pre-HotProspects/ContentView.swift
+++ Pre-HotProspects/Pre-HotProspects/Pre-HotProspects/ContentView.swift
@@ -29,15 +29,38 @@ struct DisplayView: View {
 
 struct ContentView: View {
     @StateObject private var user = User()
+    @State private var selectedTab = "One"
     
     var body: some View {
-        
+        /* Reading custom values from the environment with @EnvironmentObject */
+        /*
         VStack {
             EditView()
             DisplayView()
         }
         .environmentObject(user)
         .padding()
+         */
+        
+        /* Creating tabs with TabView and tabItem() */
+        TabView(selection: $selectedTab) {
+            Text("Tab 1")
+                .tabItem{
+                    Label("One", systemImage: "star")
+                }
+                .onTapGesture {
+                    selectedTab = "Two"
+                }
+                .tag("One")
+            Text("Tab 2")
+                .tabItem {
+                    Label("Two", systemImage: "circle")
+                }
+                .onTapGesture {
+                    selectedTab = "One"
+                }
+                .tag("Two")
+        }
     }
 }
 
